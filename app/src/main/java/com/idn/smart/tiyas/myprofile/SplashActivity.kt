81227bbc28d62ae91bafe7cb07e8beb27d762964
TiyasAria <tiyasaria123@gmail.com>
2020-09-18 17:09:31 +0700

    upload project

---
+++ app/src/main/java/com/idn/smart/tiyas/myprofile/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.idn.smart.tiyas.myprofile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        button1.setOnClickListener {
+            val intent = Intent (this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
